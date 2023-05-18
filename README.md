c7458c2bb63053d3551f3c3d8bcfd0a66e1142a5
niimisyuto <niimi@aiseis.jp>
2023-05-18 10:44:34 +0900

    [add] font-data追加

--- README.md
+++ README.md
@@ -4,11 +4,12 @@
 
 - git
 - jdk 11.0.8
-- androidstudio 33 tiramisu, adb, gradle 7.5.1 all 
+- androidstudio 33 tiramisu, adb, gradle 7.5.1 all
 - node 16.20 (nvm)
 - watchman
 
 ## パス通す
+
 - $env=JAVA_HOME = jdk11
 - $env=ANDROID_HOME = android\sdk
 - $env:Path = android\sdk\platform
@@ -22,16 +23,18 @@ npm i
 
 ## 実行
 
-### metroを起動 
+### metro を起動
 
 ```shell
 npx react-native start
 ```
+
 ### bundle + android にコンパイル
 
 ```shell
 a
 ```
-### react-native debugモード
 
-- android端末上でctrl + m
+### react-native debug モード
+
+- android 端末上で ctrl + m
--- src/router.js
+++ src/router.js
@@ -1,13 +1,12 @@
-/* eslint-disable react/no-unstable-nested-components */
 import * as React from 'react';
 
-import {StyleSheet, Image, View} from 'react-native';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import { StyleSheet, Image, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/Ionicons';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {createStackNavigator} from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 
 import Viz1 from './screens/Viz1';
 import Viz2 from './screens/Viz2';
@@ -42,7 +41,7 @@ function HomeStackScreen() {
       <HomeStack.Screen
         name="Details"
         component={CardDetails}
-        options={({route}) => ({title: route.params.headerTitle})}
+        options={({ route }) => ({ title: route.params.headerTitle })}
       />
     </HomeStack.Navigator>
   );
@@ -59,7 +58,7 @@ export function navContainer() {
             name="Home"
             component={HomeStackScreen}
             options={{
-              tabBarIcon: ({color}) => (
+              tabBarIcon: ({ color }) => (
                 <Icon
                   name="home-outline"
                   color={color}
@@ -73,7 +72,7 @@ export function navContainer() {
             name="Viz1"
             component={Viz1}
             options={{
-              tabBarIcon: ({color}) => (
+              tabBarIcon: ({ color }) => (
                 <Icon
                   name="pulse-outline"
                   color={color}
@@ -87,7 +86,7 @@ export function navContainer() {
             name="Viz2"
             component={Viz2}
             options={{
-              tabBarIcon: ({color}) => (
+              tabBarIcon: ({ color }) => (
                 <Icon
                   name="pulse-outline"
                   color={color}
@@ -101,7 +100,7 @@ export function navContainer() {
             name="Viz3"
             component={Viz3}
             options={{
-              tabBarIcon: ({color}) => (
+              tabBarIcon: ({ color }) => (
                 <Icon
                   name="pulse-outline"
                   color={color}
@@ -126,7 +125,7 @@ const styles = StyleSheet.create({
     padding: 50,
   },
   headerImage: {
-    flex: 1,
+    /*     flex: 1, */
     resizeMode: 'contain',
     height: 60,
   },
